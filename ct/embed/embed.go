7605b7dd4b841f5f257d4667a6d6d5e048c9bf78
Jaime Pillora <dev@jpillora.com>
2015-06-10 23:03:24 +1000

    test compile

--- ct/embed/embed.go
+++ ct/embed/embed.go
@@ -9,30 +9,18 @@ import (
 	"github.com/elazarl/go-bindata-assetfs"
 )
 
-type wrapFS struct {
-	FS http.Handler
-}
-
-func (wrap *wrapFS) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// if strings.HasSuffix(r.URL.Path, ".woff") {
-	// 	log.Printf("set font")
-	// 	w.Header().Set("Content-Type", "font/woff")
-	// }
-	wrap.FS.ServeHTTP(w, r)
-}
-
 // all static/ files embedded as a Go library
 func FileSystemHandler() http.Handler {
 
 	mime.AddExtensionType(".woff", "application/x-font-woff")
 	mime.AddExtensionType(".woff2", "font/woff2")
 
-	wrapper := &wrapFS{}
+	var h http.Handler
 	if info, err := os.Stat("embed/"); err == nil && info.IsDir() {
 		log.Printf("using local fs embed/ directory")
-		wrapper.FS = http.FileServer(http.Dir("embed/"))
+		h = http.FileServer(http.Dir("embed/"))
 	} else {
-		wrapper.FS = http.FileServer(&assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, Prefix: "embed"})
+		h = http.FileServer(&assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, Prefix: "embed"})
 	}
-	return wrapper
+	return h
 }
