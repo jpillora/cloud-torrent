e75158f5e4c8351323f3129f49621cfd9486250c
boypt <1033514+boypt@users.noreply.github.com>
2019-08-08 16:46:34 +0800

    fixed key order for web config

--- static/files/js/config-controller.js
+++ static/files/js/config-controller.js
@@ -3,6 +3,19 @@
 app.controller("ConfigController", function($scope, $rootScope, storage, api) {
   $rootScope.config = $scope;
   $scope.edit = false;
+  $scope.configOrderdKey = [
+    "AutoStart",
+    "DisableEncryption",
+    "EnableSeeding",
+    "EnableUpload",
+    "IncomingPort",
+    "SeedRatio",
+    "UploadRate",
+    "DownloadRate",
+    "DownloadDirectory",
+    "WatchDirectory"
+  ];
+
   $scope.toggle = function(b) {
     $scope.edit = b === undefined ? !$scope.edit : b;
   };
