d55b18cee5a639824874a6829269ae17f1c2f1fe
boypt <1033514+boypt@users.noreply.github.com>
2019-09-03 12:39:32 +0800

    load ver

--- static/generate.sh
+++ static/generate.sh
@@ -6,4 +6,4 @@ GITVER=$(git describe --tags)
 sed -i "s/CLDVER/${GITVER}/g" ${__dir}/files/index.html
 sed -i "s/CLDVER/${GITVER}/g" ${__dir}/files/template/downloads.html
 go generate
-git checkout -- ${__dir}/files/index.html
+git checkout -- ${__dir}/files/index.html ${__dir}/files/template/downloads.html
