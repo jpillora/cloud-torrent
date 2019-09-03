9dde082c77b972e7d39930ce33b8d7f3e4175378
boypt <1033514+boypt@users.noreply.github.com>
2019-09-03 12:38:15 +0800

    dym html loads ver

--- static/generate.sh
+++ static/generate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd ${__dir}
 GITVER=$(git describe --tags)
 sed -i "s/CLDVER/${GITVER}/g" ${__dir}/files/index.html
+sed -i "s/CLDVER/${GITVER}/g" ${__dir}/files/template/downloads.html
 go generate
 git checkout -- ${__dir}/files/index.html
