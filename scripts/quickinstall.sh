1475703699c43b11c9741a26554e02ab4f1256a7
boypt <1033514+boypt@users.noreply.github.com>
2019-10-22 14:10:22 +0800

    script ask for authtication

--- scripts/quickinstall.sh
+++ scripts/quickinstall.sh
@@ -9,15 +9,7 @@ if ! command -v systemctl >/dev/null 2>&1; then
     echo "> Sorry but this scripts is only for Linux with systemd, eg: Ubuntu 16.04+/Centos 7+ ..."
     exit 1
 fi
-HOSTIP=""
-for IP in $(hostname --all-ip-address); do
-  if [[ $IP == ::* ]]; then continue; fi
-  if [[ $IP == fe80* ]]; then continue; fi
-  if [[ $IP == 127* ]]; then continue; fi
-  HOSTIP=$IP
-  break
-done
-
+HOSTIP=$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
 CLDBIN=/usr/local/bin/cloud-torrent
 OSARCH=$(uname -m)
 case $OSARCH in 
@@ -39,6 +31,14 @@ case $OSARCH in
         ;;
 esac
 
+read -p "Need authentication? (Y/N)" NEEDAUTH
+USERNAME="(none)"
+PASSWORD="(none)"
+if [[ ${NEEDAUTH^^} == "Y" ]]; then
+    read -p "Input Username:" USERNAME
+    read -p "Input Password:" PASSWORD
+fi
+
 systemctl stop cloud-torrent || true
 wget -qO- https://api.github.com/repos/boypt/simple-torrent/releases/latest \
 | grep browser_download_url | grep "$BINTAG" | cut -d '"' -f 4 \
@@ -46,6 +46,11 @@ wget -qO- https://api.github.com/repos/boypt/simple-torrent/releases/latest \
 chmod 0755 ${CLDBIN}
 
 wget -O /etc/systemd/system/cloud-torrent.service https://raw.githubusercontent.com/boypt/simple-torrent/master/scripts/cloud-torrent.service
+
+if [[ ${NEEDAUTH^^} == "N" ]]; then
+    sed -i "s/ -a 'user:ctorrent'//" /etc/systemd/system/cloud-torrent.service 
+fi
+
 systemctl daemon-reload
 systemctl start cloud-torrent
 systemctl enable cloud-torrent
@@ -58,8 +63,8 @@ Open browser to http://${HOSTIP}:3000/ now!
 
 * Default DownloadDirectory: /root/downlods
 * Default Config file: /root/cloud-torrent.json
-* Default Username: user
-* Default Password: ctorrent
+* Default Username: ${USERNAME}
+* Default Password: ${PASSWORD}
 
 Read the wiki page about changing the default settings.
     https://github.com/boypt/simple-torrent/wiki/AuthSecurity
