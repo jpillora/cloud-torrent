68894c19d37e36c5ee75daed9f940bbe32dfb4d9
boypt <1033514+boypt@users.noreply.github.com>
2021-05-21 09:46:43 +0800

    deleting

--- static/files/js/downloads-controller.js
+++ static/files/js/downloads-controller.js
@@ -107,9 +107,10 @@ app.controller("NodeController", function ($scope, $rootScope, $http, $timeout) 
   };
 
   $scope.remove = function () {
+    $scope.deleting = true;
     $http.delete("download/" + encodeURIComponent(n.$path))
       .finally(function () {
-        $scope.$apply();
+        $scope.deleting = false;
       });
   };
 
