ad7083551507c6372d9dace291e3da91791cd1e0
boypt <1033514+boypt@users.noreply.github.com>
2021-06-25 13:52:53 +0800

    fix magadd tpl

--- static/generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-GITVER=$(git describe --tags)
-sed -i "s/CLDVER/${GITVER}/g" \
-    files/index.html \
-    files/template/magadded.html \
-    files/template/downloads.html \
-    files/css/app.css
-
