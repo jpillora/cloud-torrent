efab0544454daec6257638c5e8ee49bc75792c84
boypt <1033514+boypt@users.noreply.github.com>
2021-06-30 13:54:00 +0800

    isolate logger

--- engine/utils.go
+++ engine/utils.go
@@ -3,10 +3,7 @@ package engine
 import (
 	"bufio"
 	"errors"
-	"fmt"
 	"io"
-	stdlog "log"
-	"os"
 	"strings"
 	"sync"
 
@@ -44,51 +41,6 @@ func rateLimiter(rstr string) (*rate.Limiter, error) {
 		rateSize = int(v)
 	}
 	return rate.NewLimiter(rate.Limit(rateSize), rateSize*3), nil
-}
-
-var (
-	log *filteredLogger
-)
-
-type filteredLogger struct {
-	logger *stdlog.Logger
-}
-
-func (f *filteredLogger) filteredArg(v ...interface{}) []interface{} {
-	for idx, arg := range v {
-		if s, ok := arg.(string); ok && len(s) == 40 {
-			v[idx] = fmt.Sprintf("[%s...]", s[:6])
-		}
-		if s, ok := arg.(taskType); ok {
-			if s == taskTorrent {
-				v[idx] = "[Torrent]"
-			} else {
-				v[idx] = "[Magnet]"
-			}
-		}
-	}
-
-	return v
-}
-
-func (f *filteredLogger) Println(v ...interface{}) {
-	f.logger.Println(f.filteredArg(v...)...)
-}
-func (f *filteredLogger) Printf(format string, v ...interface{}) {
-	f.logger.Printf(format, f.filteredArg(v...)...)
-}
-func (f *filteredLogger) Fatal(v ...interface{}) {
-	f.logger.Fatal(f.filteredArg(v...)...)
-}
-
-func init() {
-	log = &filteredLogger{
-		logger: stdlog.New(os.Stdout, "[engine]", stdlog.LstdFlags|stdlog.Lmsgprefix),
-	}
-}
-
-func SetLoggerFlag(flag int) {
-	log.logger.SetFlags(flag)
 }
 
 func cmdScanLine(p io.ReadCloser, wg *sync.WaitGroup, logprefix string) {
